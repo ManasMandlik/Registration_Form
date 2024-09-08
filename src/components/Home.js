681566cbd6036ea5cc87cd866e9dd4dc607d2ed4
ManasMandlik <manasmandlik4@gmail.com>
2024-09-09 00:07:07 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ function Home() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
-      <button onClick={handlelogin}> Login </button>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handlelogin}> Login </button>
     </div>
   );
 }
